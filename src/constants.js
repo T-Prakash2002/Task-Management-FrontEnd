e4a60cfce8d4125f14966f9188cee8c71b140d3f
T-Prakash2002 <prakasht1405@gmail.com>
2024-05-14 11:19:24 +0530

    uri

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 
 
-export const apiuri =import.meta.env.VITE_REACT_APP_APIURI ||`http://localhost:4000`;
+export const apiuri =import.meta.env.VITE_REACT_APP_APIURI;
 
 
 // export const apiuri =`http://localhost:4000`;
